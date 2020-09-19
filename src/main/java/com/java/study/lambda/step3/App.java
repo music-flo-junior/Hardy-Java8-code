5ce8744896e03b1c8b2b18a1bb4f96c9ad124114
mingooddev <mingood92@gmail.com>
2020-09-19 18:08:21 +0900

    W1: 람다 표현식 - method reference

---
+++ src/main/java/com/java/study/lambda/step3/App.java
@@ -0,0 +1,38 @@
+package com.java.study.lambda.step3;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+/**
+ * 설명 : XXXXXXXXXXX
+ *
+ * @author Hardy(조민국) / dev.mingood@sk.com
+ * @since 2020. 09. 19
+ */
+public class App {
+
+    public static void main(String[] args) {
+        UnaryOperator<String> hi1 = (s) -> "hi " + s;
+        UnaryOperator<String> hi2 = Greeting::hi;
+        System.out.println(hi1.apply("hardy1"));
+        System.out.println(hi2.apply("hardy2"));
+
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
+        System.out.println(hello.apply("hardy3"));
+
+        Supplier<Greeting> greetingSupplier = Greeting::new;
+        Greeting greeting1 = greetingSupplier.get();// 이 시점에 만듦
+        System.out.println(greeting1.hello("supplier"));
+
+        Function<String, Greeting> greetingFunction = Greeting::new;
+        System.out.println(greetingFunction.apply("HARDY").hello("function"));
+
+        String[] names = {"hardy", "groot", "java8"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
+    }
+
+}
---
+++ src/main/java/com/java/study/lambda/step3/Greeting.java
@@ -0,0 +1,26 @@
+package com.java.study.lambda.step3;
+
+/**
+ * 설명 : XXXXXXXXXXX
+ *
+ * @author Hardy(조민국) / dev.mingood@sk.com
+ * @since 2020. 09. 19
+ */
+public class Greeting {
+    private String name;
+
+    public Greeting() {
+    }
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+    public static String hi(String name) {
+        return "hi " + name;
+    }
+}
